495cb40d1527eb056c727f69f5a9b81469abf6f1
Aditya Sah <er.adityasah@gmail.com>
2025-02-01 13:46:04 +0530

    Add Example class with nested recursion method

---
+++ Recursion/NestedRecursion/Example.java
@@ -0,0 +1,15 @@
+package Recursion.NestedRecursion;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println(fun(95));
+    }
+
+    static int fun(int n) {
+        if (n > 100) {
+            return n - 10;
+        } else {
+            return fun(fun(n + 11));
+        }
+    }
+}
